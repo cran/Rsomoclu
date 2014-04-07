65a33a8da7c9b6e02f75cd4d0d5646e194a19e84
Shichao Gao <xgdgsc@gmail.com>
2014-04-07 07:46:29 +0000

    version 1.3

---
+++ R/Rsomoclu.R
@@ -0,0 +1,25 @@
+#require(Rcpp)
+.onLoad <- function(lib, pkg){
+    library.dynam("Rsomoclu", pkg, lib)
+}
+Rsomoclu.train <-
+  function(input_data, nEpoch, 
+           nSomX, nSomY,
+           radius0, radiusN,
+           radiusCooling, scale0, scaleN,
+           scaleCooling, snapshots,
+           kernelType, mapType
+#            initialCodebookFilename
+  )
+  {
+    ## if(!is.loaded("Rtrain", PACKAGE = "Rsomoclu")){
+      
+    ## }
+    res <- .Call("Rtrain", input_data, nEpoch,
+                 nSomX, nSomY, radius0, radiusN,
+                 radiusCooling, scale0, scaleN,
+                 scaleCooling, snapshots, kernelType,
+#                  mapType, initialCodebookFilename)
+                mapType)
+    res
+  }
